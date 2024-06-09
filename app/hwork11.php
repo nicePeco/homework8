ca886541c3e24ee43d97b1db4626956994652267
nicePeco <niklavsrauza@gmail.com>
2024-06-09 14:24:43 +0300

    update 09.06

--- app/hwork11.php
+++ app/hwork11.php
@@ -5,14 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        table {
-        border: 1px solid black; 
-        }
-        table th {
-        border: 1px solid black; 
-        }
-        table td {
-        border: 1px solid black; 
+        table, td {
+            border: 1px solid;
         }
     </style>
 </head>
